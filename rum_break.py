331c82637b0fd9de43516ea49368ea0ee0ca720e
Malte Bergström <malte.bergstrom@learnet.se>
2024-12-13 14:40:54 +0100

    spel

---
+++ rum_break.py
@@ -0,0 +1,52 @@
+import random 
+print("du vakna upp i ett omt rum med två dörrar försök välja rätt dörr för att överlva ")
+spelarens_val1=input("välj dörr 1 eller 2"):
+
+if (spelarens_val1=="dörr 1"):
+    print ("du valde dörr 1 och blev uppäten av en ko")
+elif: 
+    print("du valde dörr 2 och överlevde ")
+
+spelarens_val1=input("till din överaskan så var det exakt samma rum på andra sidan dörren välj dörr 1 eller 2 igen"):
+if (spelarens_val1=="dörr 2"):
+    print ("du valde dörr 2 och överlevde")
+elif: 
+    print("du valde dörr 1 och en zombie döda dig")
+
+spelarens_val1=input("denna gången var rummet blått men exakt samma sak hände välj dörr 1 eller 2"):
+if (spelarens_val1=="dörr 1"):
+    print ("du kom igenom överlevandes")
+elif: 
+    print("en drake döda dig med ett svärd")
+
+spelarens_val1=input(""):
+if (spelarens_val1==""):
+    print ("")
+elif: 
+    print(" ")
+
+spelarens_val1=input(""):
+if (spelarens_val1==""):
+    print ("")
+elif: 
+    print("  ")
+
+spelarens_val1=input(""):
+if (spelarens_val1==""):
+    print ("")
+elif: 
+    print(" ")
+
+spelarens_val1=input(""):
+if (spelarens_val1==""):
+    print ("")
+elif: 
+    print(" ")
+
+spelarens_val1=input(""):
+if (spelarens_val1==""):
+    print ("")
+elif: 
+    print(" ")
+
+
